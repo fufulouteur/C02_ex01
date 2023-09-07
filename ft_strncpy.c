2064ef0f245f8065f29faee8dc3b4663dcbb0e6e
fufulouteur <manzodqb@icloud.com>
2023-09-07 06:45:19 +0000

    fini

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-char  *ft_strcpy(char *dest, char *src)
-{
+char *ft_strncpy(char *dest, char *src, unsigned int n) {
   char *dest_forward;
 
   dest_forward = dest;
-  while(*src != '\0')
-  {
+  while (n != 0 && *src != '\0') {
     *dest_forward = *src;
     dest_forward++;
     src++;
+    n--;
   }
 
+  while (n != 0) {
+    *dest_forward = '\0';
+    n--;
+  }
   return dest;
 }
--- main.c
+++ main.c
@@ -1,12 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+int ft_strlen(char *str);
 void ft_putstr(char *str);
-char  *ft_strcpy(char *dest, char *src);
+char *ft_strncpy(char *dest, char *src, unsigned int n);
 
-int main(void)
-{
+int main(void) {
   char str[] = "hello world\n\0";
-  char str2[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-  ft_putstr(ft_strcpy(str2, str));
+  char str2[] = "\0\0\0\0\0\0\0\0\0\0\0\n";
+  ft_putstr(ft_strncpy(str2, str, 19));
   ft_putstr(str2);
   return (0);
 }
